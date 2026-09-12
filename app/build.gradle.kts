6f44fbab013b4b4c64b4e87738952411c1f95cb5
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-12 13:05:22 +0800

    feat: fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.fati_market"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
