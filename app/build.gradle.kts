2a8bd22e5c1f02022cf99066f1f1b7225fd28a1d
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-16 13:20:18 +0800

    feat: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.fati_market"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
